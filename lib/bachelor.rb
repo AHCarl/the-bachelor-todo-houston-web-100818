c0a5a5591b9b26ff6f5948f6576181fe1bbcc32e
Alexander Carlin <ahcarlin@gmail.com>
2018-10-19 19:10:18 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -46,15 +46,15 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  sum = 0.0
+  sum = 0
   length = 0.0
   data.each do |k,v|
     if k == season
       length = v.count
       v.each do |k2, v2| 
-        sum += k2["age"]
+        sum += k2["age"].to_i
       end 
     end
   end
-  (sum / length).round
+  (sum.to_f / length).round
 end
